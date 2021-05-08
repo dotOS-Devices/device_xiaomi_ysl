35fbe9baf863c3767e874c385be5a60e134eeffa
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-05-08 10:52:55 +0200

    ysl: Add Pixel Charger

--- dot_ysl.mk
+++ dot_ysl.mk
@@ -34,7 +34,7 @@ $(call inherit-product, vendor/dot/config/common.mk)
 DOT_OFFICIAL=true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
-
+TARGET_INCLUDE_PIXEL_CHARGER := true
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
 PRODUCT_NAME := dot_ysl
